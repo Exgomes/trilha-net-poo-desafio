687ab5aadd76a808c65ed53f7a34231428e5c355
Exgomes <ewelinesamon@hotmail.com>
2023-12-21 22:47:03 -0300

    Desafio concluidögit statusgit status

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
-    {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+
+    public class Iphone : Smartphone
+    {  
+          // TODO: Sobrescrever o método "InstalarAplicativo"
+          public Iphone(string numero, string modelo, string imei, string memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            // Construtor...
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} na App Store...");
+        }
+       
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456789", modelo: "Modelo 5s", imei: "785463", memoria: "64");
+nokia.Ligar();
+nokia.InstalarAplicativo("Ifood");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "12322", modelo: "Modelo 15 pro max", imei: "12344", memoria: "128");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Uber");
